76610a2c9bf0ab7b6a418e003b1ce40a2f59e9e1
barakis <Barakis45>
2016-11-28 11:55:01 -0600

    Updated Dice Rolling Rules

--- src/Systems/NWOD.kt
+++ src/Systems/NWOD.kt
@@ -2,12 +2,15 @@ package Systems
 
 import library.Dice
 import library.DiceTypesK
-import library.IDie
 
 /**
- * Created by barakis on 11/14/16.
+ * Rules:
+ *    1. roll about 8 = +1 success and roll = 10 +1 success plus re-roll unless Second Chance is active
+ *    2. Second Chance : Dice Pool has to be 0. 10 is the only success and dramatic fail is when 1 is rolled
+ *                       Only time that rolling a 1 is BAD
+ *    3. NWOD all dice can explode if they roll a 10 but certain situation(weapons) can cause the TN to drop to a 9
  */
-class NWOD(private val size : Int, private val exploded : Boolean = false, private val explodeTN : Int = 0) : IWhiteWolf
+class NWOD(private val size : Int, private val explodeTN : Int = 0) : IWhiteWolf
 {
     override val type: DiceTypesK
         get() = DiceTypesK.D10
@@ -15,7 +18,7 @@ class NWOD(private val size : Int, private val exploded : Boolean = false, priva
     override val targetNumber: Int
         get() = 8
 
-    override var isExploding: Boolean = exploded
+    override var isExploding: Boolean = true
 
     override var explodingTargetNumber: Int = explodeTN
 
@@ -23,15 +26,81 @@ class NWOD(private val size : Int, private val exploded : Boolean = false, priva
 
     val Size : Int = size
 
-    var onesRolled : Int = 0
-
     var isFail : Boolean = false
 
-    private val Dice : Array<IDie> = Array<IDie>(Size,{Dice(DiceTypesK.D10)})
+    var dramaticFail : Boolean = false
+
+    private var Dice : MutableList<Dice> = mutableListOf(Dice(type))
+
+    val dicePool : List<Dice> = Dice.toList()
+
+    init
+    {
+        val D : Dice = Dice(type)
+        when
+        {
+            size == 0 ->
+            {
+                Dice.add(D)
+            }
+            else ->
+            {
+                (0..Size-1).forEach { i ->
+                    Dice.add(D)
+                }
+            }
+
+        }
+    }
 
     fun RollDice()
     {
+        // 2nd Chance Rule
+        // NORMAL NWOD RULES BELOW
+        when (size) {
+            0 -> {
+                var D: Dice = Dice(type)
+                D.RollDie()
+                when {
+                    D.totalRolled == 1 -> dramaticFail = true
+                    D.totalRolled == 10 -> {
+                        do {
+                            totalSuccess++
+                            D.RollDie()
+                        } while (D.totalRolled == 10)
+                    }
+                }
+            }
+            else -> Dice.forEach()
+            {
+                totalSuccess += RollDiceExplode(it)
+            }
+        }
+        if (totalSuccess == 0) isFail = true else isFail = false
+    }
 
+
+    private fun RollDiceExplode(D : Dice) : Int
+    {
+        D.RollDie()
+        var rolled = D.totalRolled
+        var results = 0
+
+        when(rolled)
+        {
+            8 -> results++
+            explodingTargetNumber -> {
+                results++
+                results += RollDiceExplode(D)
+            }
+            10 -> {
+                results++
+                results += RollDiceExplode(D)
+            }
+            else -> return 0
+        }
+
+        return results
     }
 
 }
